dce73d09d975deed2f7a411ec7c2e12e67b24e08
Sunil Bagde <bagdesunil786@gmail.com>
2021-10-15 22:10:26 +0530

    selection sort 1

---
+++ sorting-algorithms/01-selection-sort.js
@@ -0,0 +1,23 @@
+function selectionSort(nums,n) {
+  for(let i = 0; i< n-1; i++) {
+     let min = i;
+     for(let j = i+1; j < n ; j++ ) {
+       if(nums[j] < nums[min]) {
+         min = j;
+       }
+     }
+     let temp = nums[i]
+     nums[i] =  nums[min]
+     nums[min] = temp
+  }
+  return nums;
+}
+const nums = [2,7,4,1,5,3]
+const result =  selectionSort(nums, nums.length);
+console.log("result", result);
+/*
+ 2 5 3 6 4 9
+ 2 5 3 6 4 9
+ 2,3,5,6,4,9
+
+*/
