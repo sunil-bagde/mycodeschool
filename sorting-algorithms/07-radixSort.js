e7fe34be1da2cb7e142625c896353453939eb9ba
Sunil Bagde <bagdesunil786@gmail.com>
2021-10-19 22:23:25 +0530

    radix sort

---
+++ sorting-algorithms/07-radixSort.js
@@ -0,0 +1,63 @@
+function getDigit(number, place, longestNumber) {
+  const string = number.toString();
+  const size = string.length;
+
+  const mod = longestNumber - size;
+  return string[place - mod] || 0;
+}
+
+function findLongestNumber(array) {
+  let longest = 0;
+  for (let i = 0; i < array.length; i++) {
+    const currentLength = array[i].toString().length;
+    longest = currentLength > longest ? currentLength : longest;
+  }
+  return longest;
+}
+
+function radixSort(array) {
+  const longestNumber = findLongestNumber(array);
+
+  const buckets = new Array(10).fill().map(() => []); // make an array of 10 arrays
+
+  for (let i = longestNumber - 1; i >= 0; i--) {
+    while (array.length) {
+      const current = array.shift();
+      buckets[getDigit(current, i, longestNumber)].push(current);
+    }
+
+    for (let j = 0; j < 10; j++) {
+      while (buckets[j].length) {
+        array.push(buckets[j].shift());
+      }
+    }
+  }
+
+  return array;
+}
+
+const nums = [
+  20,
+  51,
+  3,
+  801,
+  415,
+  62,
+  4,
+  17,
+  19,
+  11,
+  1,
+  100,
+  1244,
+  104,
+  944,
+  854,
+  34,
+  3000,
+  3001,
+  1200,
+  633,
+];
+const ans = radixSort(nums);
+console.log("ans", ans);
